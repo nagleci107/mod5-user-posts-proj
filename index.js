3b3914cbef3fd1d5234bbee7b109a5e41463038f
nagleci107 <nagleci107@gmail.com>
2026-07-13 13:07:04 -0400

    add dynamic user cards

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-APIs:
-
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
+
+
+async function main () {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData = await users.json();
+    const userListEl = document.querySelector(".user-list");
+
+   userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
+}
+
+main();
+
+function showUserPosts(id) {
+    window.location.href = `/user.html`
+    console.log(id);
+}
+
+function userHTML(user) {
+    return `<div class="user-card" onclick="showUserPosts(${user.id})">
+        <div class="user-card__container">
+        <h3>${user.name}</h3>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+        </div>
+    </div>`;
+}
