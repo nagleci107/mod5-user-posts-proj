7bae86becf632e14feb71aa03823a219f5ceb12f
nagleci107 <nagleci107@gmail.com>
2026-07-13 13:42:53 -0400

    add search functionality

--- user.js
+++ user.js
@@ -1,7 +1,12 @@
 const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
 
-async function main() {
-    const id = localStorage.getItem("id");
+async function onSearchChange(event) {
+    const id = event.target.value;
+    renderPosts(id)
+}
+
+async function renderPosts(id) {
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)    
     const postsData = await posts.json();
 
@@ -19,4 +24,4 @@ function postHTML(post) {
     </div>`;
 }
 
-main();
+renderPosts(id);
